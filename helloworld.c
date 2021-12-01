182c6d361a0496641ad53df217f8fba41a6176d5
trannhatbandev <trannhatban34@gmail.com>
2021-12-01 16:17:14 +0700

    Second Commit

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-	printf("Hello World! This is project of Tran Nhat Ban")
+	printf("Hello World! This is project of Tran Nhat Ban\n");
+	printf("Day la Tran Nhat Ban\n");
 	return 0;
 
 }
